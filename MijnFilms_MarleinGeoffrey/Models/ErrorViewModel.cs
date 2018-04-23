fef7c99ac885f5bda2284ba154bb5f219d20cd2c
MarleinGeoffrey <geoffrey.marlein@student.howest.be>
2018-04-23 09:40:04 +0200

    Add project files.

---
+++ MijnFilms_MarleinGeoffrey/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MijnFilms_MarleinGeoffrey.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
